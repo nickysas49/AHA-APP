0a97698bbf0e79f54849cb792028dd9abd0fd477
nicky <nixinyuan0925@gmail.com>
2022-01-10 16:15:25 +1100

    second commit

--- public/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-function add_fl(){
-    var button_fl_count = document.getElementById('button_fl_count');
-    var count = button_fl_count.value;
-    count ++
-    button_fl_count.value = count
-    console.log(count)
-  }
